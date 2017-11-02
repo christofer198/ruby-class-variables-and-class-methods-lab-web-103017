9942c79fcc89d17436ac502bbcc0e7f28276ddd9
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-02 12:40:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,6 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    artist_count = 0
     @@artists << artist
     @@genres << genre
   end
@@ -39,10 +38,10 @@ class Song
   def self.artist_count
     artist_count = {}
    @@artists.each do |artist|
-     if !artist_count.has_key?(artist)
-       artist_count[artist] = 1
-     else
+     if artist_count.has_key?(artist) == true
        artist_count[artist] += 1
+     else
+       artist_count[artist] = 1
      end
    end
    artist_count
